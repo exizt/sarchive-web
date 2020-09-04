6b823773e115d941840f44a56d4e785cacd0fd54
e2xist <e2xist@gmail.com>
2020-09-04 12:53:01 +0900

    update comments

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -6,6 +6,11 @@ use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 
+/**
+ * Http 접속을 Https 로 redirect 시키는 클래스.
+ * 클라우드 플레어 자체에서 always https 설정이 되어있으면, 이 기능이 필요하지는 않음. (동일한 기능)
+ * 가끔 always https 설정이 꺼질 때가 있으므로 그 때를 대비하는 코드.
+ */
 class HttpsProtocol {
 	// HTTPS 로 제공하지 않을 URI. legacy 로 시작되는 URI 일 경우 SSL 을 강제 적용하지 않음
     protected $except = [
@@ -18,8 +23,8 @@ class HttpsProtocol {
 	];
 	
 	/**
-	 * 참고 주소 http://stackoverflow.com/questions/28402726/laravel-5-redirect-to-https
-	 * https://www.lesstif.com/php-and-laravel/https-27984776.html
+	 * 참고 주소 https://www.lesstif.com/php-and-laravel/https-27984776.html
+	 * http://stackoverflow.com/questions/28402726/laravel-5-redirect-to-https
 	 * @param Request $request
 	 * @param Closure $next
 	 * @return \Illuminate\Http\RedirectResponse
