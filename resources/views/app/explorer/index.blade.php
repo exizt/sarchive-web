f8bb37f7c7f4f2035cc3f1897bc779b9e77295b3
SH.H <e2xist@gmail.com>
2021-01-27 03:10:28 +0900

    minor

--- resources/views/app/explorer/index.blade.php
+++ resources/views/app/explorer/index.blade.php
@@ -38,7 +38,7 @@
                         @endif
                             >
                             <div class="d-flex w-100 justify-content-between">
-                                <h5 class="mb-1">{{ $item->title }}</h5>
+                                <h5 class="sarc-item-label">{{ $item->title }}</h5>
                                 <small>{{ $item->created_at->format('Y-m-d') }}</small>
                             </div>
                             <p class="mb-1 pl-md-3 sarc-item-comments">
