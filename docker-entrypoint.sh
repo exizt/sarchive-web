5ac773aea5f91e7d55fceb199b24b4a50e2efc89
SH.H <e2xist@gmail.com>
2021-09-08 00:49:26 +0900

    도커로 사용할 수 있게 작업함.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,26 +4,36 @@
 # dockerize -wait tcp://db:3306 -timeout 20s
 # dockerize -wait tcp://data-api:5000 -timeout 20s
 
-cd /app/web
+
+cd /app
 
 if [ -f composer.json ]; then
-    echo "laravel project exists"
-
     if [ -d vendor ]; then
         echo "vendor exists"
-
+        # vendor 가 있으므로 composer install 을 하지 않고 진행
     else
-        echo "install composer packages"
-        # composer install -vv
+        if [ -d "/app/web/bootstrap/cache" ]; then
+            # 여기에 해당한느 게 있으면 삭제를 먼저 하지 않으면 
+            # php artisan package:discover --ansi handling the post-autoload-dump event returned with error code 1
+            # 오류를 만날 수 있다.
+            rm -f /app/web/bootstrap/cache/packages.php
+            rm -f /app/web/bootstrap/cache/services.php
+        fi
+        composer install
+
+        # 처음 구동일 것이라고 가정하고. 여기서 서버 네임 지정하는 부분 추가.
+        echo "ServerName localhost" >> /etc/apache2/apache2.conf
     fi
 else
-    echo "laravel project doesn't exists..."
-    # 종료하는 거를 넣어야 함...
+    echo "composer.json not exists"
 fi
 
-# service apache2 stop
 
 # php artisan migrate
+
+# 아파치 서버 시작 관련
+# service apache2 stop
+
 echo "Apache server is running..."
 source /etc/apache2/envvars
-apache2 -DFORGROUND
+exec apache2 -DFOREGROUND
--- local.Dockerfile
+++ local.Dockerfile
@@ -1,13 +1,17 @@
-FROM php:7.3-apache-buster
+FROM php:7.4-apache-buster
 
 WORKDIR /app
 
 # 필요한 것들 설치
-# - wget (dockerize에 필요)
-# RUN  apt-get update \
-#    && apt-get install -y wget \
-#    && apt-get autoremove -y \
-#    && rm -rf /var/lib/apt/lists/*
+# - wget : dockerize에서 필요
+# - git, unzip : composer 에서 필요
+RUN  apt-get update \
+    && apt-get install -y \
+    wget \
+    git \
+    unzip \
+    && apt-get autoremove -y \
+    && rm -rf /var/lib/apt/lists/*
 
 # DB 연결에 대기시킬 수 있는 기능을 하는 Dockerize 를 이용
 # ENV DOCKERIZE_VERSION v0.6.1
@@ -34,13 +38,20 @@ EXPOSE 80
 # 도커에서 dforground 옵션으로 동작시킬 것이기 때문에 서비스에서는 종료.
 RUN service apache2 stop
 
+# 웹 어플리케이션 지정
 COPY ./vhost.conf /etc/apache2/sites-available/000-default.conf
+
+# entrypoint.sh 복사
 COPY docker-entrypoint.sh .
 
-# vendor 속도 저하 이슈를 해결하기 위해서 해당하는 부분을 copy
-COPY ./web/public ./
-COPY ./web/server.php .
+# composer 설정 및 실행
+COPY ./web/composer.json .
+# COPY ./web/composer.lock .
+ENV COMPOSER_ALLOW_SUPERUSER 1
+# RUN composer install
+# RUN composer install --optimize-autoloader --no-dev
 
+ENV C_VENDOR_ROOT='/app'
 
 # 
 WORKDIR /app/web
