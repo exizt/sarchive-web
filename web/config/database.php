53549b363a741f0ce1c7ea2d62e57fecc93f1ac9
SH.H <e2xist@gmail.com>
2021-09-13 10:05:23 +0900

    db 설정 조금 변경.

--- web/config/database.php
+++ web/config/database.php
@@ -52,7 +52,7 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
-        'mysql_chosimlogin' => [
+        'shared_login_db' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
