c44b98ac6f5aceb26bc5a37deca067c03f0ef101
exizt <e2xist@gmail.com>
2022-02-07 18:35:23 +0900

    minor

--- web/public/index.php
+++ web/public/index.php
@@ -6,10 +6,6 @@ if (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) {
 	$_SERVER["REMOTE_ADDR"] = $_SERVER["HTTP_CF_CONNECTING_IP"];
 }
 
-# vendor의 ROOT 경로
-$cc_vendor_root = getenv('C_VENDOR_ROOT') ?: __DIR__.'/..';
-define('C_VENDOR_ROOT', $cc_vendor_root);
-
 # 원래 부분 시작
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
@@ -42,7 +38,7 @@ if (file_exists(__DIR__.'/../storage/framework/maintenance.php')) {
 |
 */
 
-require C_VENDOR_ROOT.'/vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
--- web/resources/lang/ko/validation.php
+++ web/resources/lang/ko/validation.php
@@ -81,7 +81,7 @@ return [
     'max' => [
         'numeric' => 'The :attribute must not be greater than :max.',
         'file' => 'The :attribute must not be greater than :max kilobytes.',
-        'string' => 'The :attribute must not be greater than :max characters.',
+        'string' => ':attribute 는 :max 글자수보다 적어야 합니다.',
         'array' => 'The :attribute must not have more than :max items.',
     ],
     'mimes' => 'The :attribute must be a file of type: :values.',
