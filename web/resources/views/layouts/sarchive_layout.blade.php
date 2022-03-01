42e2cf88798f3997fbb3c28e3cf6d9e24f4c9125
exizt <e2xist@gmail.com>
2022-03-02 00:47:23 +0900

    indent

--- web/resources/views/layouts/sarchive_layout.blade.php
+++ web/resources/views/layouts/sarchive_layout.blade.php
@@ -35,49 +35,49 @@
 </head>
 <body @isset($bodyParams) @foreach ($bodyParams as $k => $v) data-{{$k}}="{{$v}}" @endforeach @endisset >
   <header>
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-          <a id="logo" class="navbar-brand mr-2" href="/"></a>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a id="logo" class="navbar-brand mr-2" href="/"></a>
 
-          @isset($layoutParams['archiveId'])
-          <a class="navbar-brand" href="/archives/{{$layoutParams['archiveId']}}">
-              {{$layoutParams['archiveName'] ?? 'S아카이브'}}
-          </a>
-          @else
-          <a class="navbar-brand" href="/">S아카이브</a>
-          @endisset
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-          </button>
+      @isset($layoutParams['archiveId'])
+      <a class="navbar-brand" href="/archives/{{$layoutParams['archiveId']}}">
+          {{$layoutParams['archiveName'] ?? 'S아카이브'}}
+      </a>
+      @else
+      <a class="navbar-brand" href="/">S아카이브</a>
+      @endisset
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav mr-auto" id="shh-header-navbar"></ul>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto" id="shh-header-navbar"></ul>
+        @isset($layoutParams['archiveId'])
+        @if(($layoutMode ?? '') != 'first')
+        <form class="form-inline my-2 my-lg-0" action="/archives/{{ $layoutParams['archiveId'] }}/search">
+            <input class="form-control mr-sm-2 site-shortcut-key-f" type="search" placeholder="Search" aria-label="Search" name="q" value="{{ $parameters['q'] ?? ''}}">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+        @endif
+        @endisset
+        <ul class="navbar-nav">
+          <li class="nav-item dropdown"><a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink_My" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }}</a>
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink_My">
               @isset($layoutParams['archiveId'])
-              @if(($layoutMode ?? '') != 'first')
-              <form class="form-inline my-2 my-lg-0" action="/archives/{{ $layoutParams['archiveId'] }}/search">
-                  <input class="form-control mr-sm-2 site-shortcut-key-f" type="search" placeholder="Search" aria-label="Search" name="q" value="{{ $parameters['q'] ?? ''}}">
-                  <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-              </form>
-              @endif
+              <a class="dropdown-item site-shortcut-key-n site-shortcut-key-a" href="{{ route('doc.create',['archive'=>$layoutParams['archiveId']]) }}">글쓰기</a>
               @endisset
-              <ul class="navbar-nav">
-                  <li class="nav-item dropdown"><a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink_My" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }}</a>
-                      <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink_My">
-                          @isset($layoutParams['archiveId'])
-                          <a class="dropdown-item site-shortcut-key-n site-shortcut-key-a" href="{{ route('doc.create',['archive'=>$layoutParams['archiveId']]) }}">글쓰기</a>
-                          @endisset
-                          <div class="dropdown-divider"></div>
-                          <a class="dropdown-item" href="/">아카이브 변경</a>
-                          <a class="dropdown-item" href="/static/shortcut">단축키 일람</a>
-                          <div class="dropdown-divider"></div>
-                          <a class="dropdown-item" href="/admin">설정</a>
-                          <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();"><i
-                              class="fas fa-sign-out-alt" aria-hidden="true"></i>&nbsp;Logout</a>
-                          <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">{{ csrf_field() }}</form>
-                      </div>
-                  </li>
-              </ul>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="/">아카이브 변경</a>
+              <a class="dropdown-item" href="/static/shortcut">단축키 일람</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="/admin">설정</a>
+              <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();"><i
+                  class="fas fa-sign-out-alt" aria-hidden="true"></i>&nbsp;Logout</a>
+              <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">{{ csrf_field() }}</form>
           </div>
-      </nav>
+          </li>
+        </ul>
+      </div>
+    </nav>
   </header>
   <div id="layoutBody">
     @if(($layoutMode ?? '') == 'admin')
@@ -92,7 +92,7 @@
         </div>
       </div>
     @else
-    @yield('content')
+      @yield('content')
     @endif
   </div>
   <footer>
