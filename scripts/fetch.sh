b0768832e651f60880a543ad494439e4f489c26c
exizt <e2xist@gmail.com>
2023-10-02 19:25:40 +0900

    fetch.sh

--- scripts/fetch.sh
+++ scripts/fetch.sh
@@ -6,7 +6,7 @@ if [ -z "$BASH_VERSION" ]; then exec bash "$0" "$@"; exit; fi
 # 스크립트의 경로
 SCRIPT_PATH=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")") # 스크립트의 경로
 PROJECT_ROOT_PATH="${SCRIPT_PATH}/.." # 위키 프로젝트 경로
-LARAKIT_PATH="${PROJECT_ROOT_PATH}/aether-wiki" # LaraBaseKit 경로
+LARAKIT_PATH="${PROJECT_ROOT_PATH}/larabasekit" # LaraBaseKit 경로
 
 # 프로젝트 경로로 이동
 cd $LARAKIT_PATH
