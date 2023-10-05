cbacfbae72f326dfab662c592cc530a154dcfd72
exizt <e2xist@gmail.com>
2023-10-05 09:08:51 +0900

    minor

--- web/config/filesystems.php
+++ web/config/filesystems.php
@@ -68,7 +68,12 @@ return [
     | the locations of the links and the values should be their targets.
     |
     */
-
+    /*
+    | public_path('abc') => storage_path('app/public') 으로 설정되면 url로 
+    | 예를 들어, '/abc/1.txt'를 호출시 'app/public/1.txt'를 찾게끔 심볼릭 링크가 생성된다.
+    | 정확히는 'public/abc'라는 심볼릭 링크가 생성되는데, '../storage/app/public'을 가리키게 된다.
+    | 아래에서는 'storage_path('~~')는 절대경로일 가능성이 있어서, 상대경로로 심볼릭을 생성하는 부분.
+    */
     'links' => [
         // public_path('storage') => storage_path('app/public'),
     ],
