8f6c11f8e5383f00c56efcd7a225e3c741a69e09
exizt <e2xist@gmail.com>
2023-10-08 20:13:16 +0900

    bug fix

--- web/resources/views/layouts/sarchive_layout.blade.php
+++ web/resources/views/layouts/sarchive_layout.blade.php
@@ -69,9 +69,11 @@
               <a class="dropdown-item" href="/static/shortcut">단축키 일람</a>
               <div class="dropdown-divider"></div>
               <a class="dropdown-item" href="/admin">설정</a>
+              @env('local')
               <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();"><i
                   class="fas fa-sign-out-alt" aria-hidden="true"></i>&nbsp;Logout</a>
               <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">{{ csrf_field() }}</form>
+              @endenv
           </div>
           </li>
         </ul>
