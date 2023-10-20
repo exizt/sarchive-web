4ef55a741919cab8782d73514dfe29e8ccc54f19
exizt <e2xist@gmail.com>
2023-10-20 16:56:05 +0900

    minor

--- web/resources/views/layouts/sarchive_layout.blade.php
+++ web/resources/views/layouts/sarchive_layout.blade.php
@@ -19,18 +19,18 @@
 <link rel="stylesheet" href="/assets/css/archive.css">
 @stack('layout-styles')
 <!-- scripts -->
+<script type="module" src="/assets/js/app_mod.js"></script>
+<script src="/assets/js/common.js"></script>
+<script src="/assets/js/nav.js"></script>
+<script src="/assets/modules/scroll-to-top/scroll-to-top.min.js"></script>
+<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+<script src="/assets/modules/jshotkey/jshotkey.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
 <!-- popper.JS : dropdown of bootstrap 을 위해 필요. (bootstrap 4.0.0 이후로 추가) -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js" integrity="sha512-ubuT8Z88WxezgSqf3RLuNi5lmjstiJcyezx34yIU2gAHonIi27Na7atqzUZCOoY4CExaoFumzOsFQ2Ch+I/HCw==" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js" integrity="sha512-XKa9Hemdy1Ui3KSGgJdgMyYlUg1gM+QhL6cnlyTe2qzMCYm4nAZ1PsVerQzTTXzonUR+dmswHqgJPuwCq1MaAg==" crossorigin="anonymous"></script>
 <!-- ## semi modules ## -->
-<script src="/assets/modules/scroll-to-top/scroll-to-top.min.js"></script>
-<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-<script src="/assets/modules/jshotkey/jshotkey.min.js"></script>
 <script src="/assets/js/site-base.js"></script>
-<script src="/assets/js/common.js"></script>
-<script src="/assets/js/nav.js"></script>
-<script type="module" src="/assets/js/app_mod.js"></script>
 @stack('scripts')
 </head>
 <body @isset($bodyParams) @foreach ($bodyParams as $k => $v) data-{{$k}}="{{$v}}" @endforeach @endisset >
