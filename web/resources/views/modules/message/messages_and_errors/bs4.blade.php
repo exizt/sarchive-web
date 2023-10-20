fc67767f1389e1f1a7fb1aea833660cc81e4515b
exizt <e2xist@gmail.com>
2023-10-21 02:34:22 +0900

    move messages_and_errors.blade

--- web/resources/views/modules/message/messages_and_errors/bs4.blade.php
+++ web/resources/views/modules/message/messages_and_errors/bs4.blade.php
@@ -1,14 +1,19 @@
+{{-- bs4 style --}}
 @if (session()->has('message'))
 <div class="alert alert-success alert-dismissible fade show" role="alert">
-	<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+	<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+		<span aria-hidden="true">&times;</span>
+	</button>
 	<h4 class="alert-heading">알림</h4>
 	{{ session()->get('message') }}
 </div>
 @endif
 
 @if ($errors->any())
-<div class="alert alert-danger alert-dismissible fade show" role="alert">
-	<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+<div class="alert alert-danger alert-dismissible fade show"role="alert">
+	<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+		<span aria-hidden="true">&times;</span>
+	</button>
 	<h4 class="alert-heading">오류</h4>
     <ul>
         @foreach ($errors->all() as $error)
--- web/resources/views/modules/message/messages_and_errors/default.blade.php
+++ web/resources/views/modules/message/messages_and_errors/default.blade.php
@@ -1,3 +1,4 @@
+{{-- bs4 style --}}
 @if (session()->has('message'))
 <div class="alert alert-success alert-dismissible fade show" role="alert">
 	<button type="button" class="close" data-dismiss="alert" aria-label="Close">
