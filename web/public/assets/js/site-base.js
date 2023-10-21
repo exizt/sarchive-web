603400a41cd568f3680bb110dd7db7336aff6763
exizt <e2xist@gmail.com>
2023-10-22 00:15:36 +0900

    minor

--- web/public/assets/js/site-base.js
+++ web/public/assets/js/site-base.js
@@ -1,5 +1,9 @@
-$.ajaxSetup({
-    headers: {
-        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+window.onload = function() {
+    if (window.jQuery) {
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
     }
-});
+}
