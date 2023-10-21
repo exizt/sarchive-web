d81820d56de3f90bda5555cfd31578acd21efe95
exizt <e2xist@gmail.com>
2023-10-22 00:16:12 +0900

    dialogs

--- web/resources/views/modules/dialog/delete/default.blade.php
+++ web/resources/views/modules/dialog/delete/default.blade.php
@@ -1,9 +1,9 @@
-{{-- bs4 style --}}
+{{-- bs5 style --}}
 <div class="modal fade" id="modal-delete" tabIndex="-1">
     <div class="modal-dialog modal-dialog-centered modal-sm">
         <div class="modal-content">
             <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
                 <p class="lead">
@@ -13,9 +13,9 @@
             <div class="modal-footer">
                 <form method="POST"
                     action="{{ $action }}">
-                    <input type="hidden" name="_token" value="{{ $token }}"> <input
-                        type="hidden" name="_method" value="DELETE">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">취소</button>
+                    <input type="hidden" name="_token" value="{{ $token }}">
+                    <input type="hidden" name="_method" value="DELETE">
+                    <button type="button" class="btn btn-default" data-bs-dismiss="modal">취소</button>
                     <button type="submit" class="btn btn-danger">삭제</button>
                 </form>
             </div>
